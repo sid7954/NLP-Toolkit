b0060a6bcc2420d22069cf37c02e867ab4e97c15
Siddhant Garg <sid7954@gmail.com>
2018-06-10 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tmp/binary.py to NMT Toolkit

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ for filename in ~/Desktop/tmp/*.py; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Toolkit"
-    GIT_COMMITTER_DATE="Sun 19 Aug 2018 20:19:19 BST" git commit --amend --no-edit --date "Tue 28 Aug 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Sun 10 Jun 2018 20:19:19 BST" git commit --amend --no-edit --date "Sun 10 Jun 2018 20:19:19 BST"
 done
 git push origin master
