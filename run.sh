05d7b1aa4be839b05ec155ebbecff3b67f8a681e
Siddhant Garg <sid7954@gmail.com>
2018-06-11 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tmp/classifier.py to NMT Toolkit

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ for filename in ~/Desktop/tmp/*.py; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Toolkit"
-    GIT_COMMITTER_DATE="Sun 10 Jun 2018 20:19:19 BST" git commit --amend --no-edit --date "Sun 10 Jun 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Mon 11 Jun 2018 20:19:19 BST" git commit --amend --no-edit --date "Mon 11 Jun 2018 20:19:19 BST"
 done
 git push origin master
