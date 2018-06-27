afe026b5aed0d60b77dcd7c572d16fc7a98eed08
Siddhant Garg <sid7954@gmail.com>
2018-06-27 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tmp/validation.py to NMT Toolkit

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ for filename in ~/Desktop/tmp/*.py; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Toolkit"
-    GIT_COMMITTER_DATE="Wed 26 Jun 2018 20:19:19 BST" git commit --amend --no-edit --date "Wed 26 Jun 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Thu 27 Jun 2018 20:19:19 BST" git commit --amend --no-edit --date "Thu 27 Jun 2018 20:19:19 BST"
 done
 git push origin master
