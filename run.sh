a5c80cc9e7c5cba639f6c96f937cb8e586a0f109
Siddhant Garg <sid7954@gmail.com>
2018-06-29 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tmp/obj_number.txt to NMT Toolkit

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for filename in ~/Desktop/tmp/*.py; do
+for filename in ~/Desktop/tmp/*.txt; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Toolkit"
-    GIT_COMMITTER_DATE="Thu 27 Jun 2018 20:19:19 BST" git commit --amend --no-edit --date "Thu 27 Jun 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Sat 29 Jun 2018 20:19:19 BST" git commit --amend --no-edit --date "Sat 29 Jun 2018 20:19:19 BST"
 done
 git push origin master
