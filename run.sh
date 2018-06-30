1d24ac5398126d6e698db8dd43f043d6801c24e0
Siddhant Garg <sid7954@gmail.com>
2018-06-30 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tmp/past_present.txt to NMT Toolkit

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ for filename in ~/Desktop/tmp/*.txt; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Toolkit"
-    GIT_COMMITTER_DATE="Sat 29 Jun 2018 20:19:19 BST" git commit --amend --no-edit --date "Sat 29 Jun 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Sun 30 Jun 2018 20:19:19 BST" git commit --amend --no-edit --date "Sun 30 Jun 2018 20:19:19 BST"
 done
 git push origin master
