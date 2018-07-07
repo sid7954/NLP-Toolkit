7b9f61300c206b1dfb25940d11338caec8d662bc
Siddhant Garg <sid7954@gmail.com>
2018-07-07 20:19:19 +0100

    Added file /Users/Siddhant/Desktop/tmp/README.md to NMT Toolkit

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for filename in ~/Desktop/tmp/*.txt; do
+for filename in ~/Desktop/tmp/*.md; do
     cp $filename .
     git add .
     git commit -m "Added file $filename to NMT Toolkit"
-    GIT_COMMITTER_DATE="Sat 6 Jul 2018 20:19:19 BST" git commit --amend --no-edit --date "Sat 6 Jul 2018 20:19:19 BST"
+    GIT_COMMITTER_DATE="Sun 7 Jul 2018 20:19:19 BST" git commit --amend --no-edit --date "Sun 7 Jul 2018 20:19:19 BST"
 done
 git push origin master
